61c98bdbbb5e6c3c844ffff1f767025c4ac2b510
Guru1316 <guruprasadgdr1@gmail.com>
2026-05-07 16:11:30 +0530

    cors problem solved

--- backend/src/main/java/com/safetrail/backend/SecurityConfig.java
+++ backend/src/main/java/com/safetrail/backend/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://safetrial-fsd-frontend.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
