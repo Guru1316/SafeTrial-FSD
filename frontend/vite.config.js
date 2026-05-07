3ae0558c2330bc4c37236eac95ddbf43756162d5
Guru1316 <guruprasadgdr1@gmail.com>
2026-05-07 14:42:17 +0530

    API changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://safetrial-fsd-backend.onrender.com",
         changeOrigin: true,
       },
     },
